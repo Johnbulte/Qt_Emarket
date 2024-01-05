cc78c8e96e3bf9b8a000521c363b1eb37c8cf322
Johnbulte <a760862802@outlook.com>
2024-01-05 12:47:29 +0800

    增加部分功能代码

--- purchase.cpp
+++ purchase.cpp
@@ -1,8 +1,8 @@
 #include "purchase.h"
 #include "ui_purchase.h"
 #include <QSqlQuery>
-#include <QByteArray>
 #include <QDebug>
+#include <QSqlQueryModel>
 
 purchase::purchase(QWidget *parent) :
     QWidget(parent),
@@ -22,7 +22,7 @@ purchase::~purchase()
 void purchase::setvalue()
 {
     QSqlQuery queryvalue;
-    queryvalue.exec("SELECT `Name` FROM category");
+    queryvalue.exec("SELECT commodity.category FROM commodity");
 
     // 核心代码
     while (queryvalue.next())
@@ -33,3 +33,26 @@ void purchase::setvalue()
         ui->comboBox->addItems(list);
     }
 }
+
+
+
+void purchase::on_comboBox_currentTextChanged(const QString &arg1)
+{
+    // 每次执行改槽函数时将combobox_2的数据清空
+    ui->comboBox_2->clear();
+    QString sql = QString("SELECT commodity.`Name` FROM commodity WHERE commodity.category = '" + ui->comboBox->currentText() + "'");
+
+    QSqlQuery sqlsearch;
+    sqlsearch.exec(sql);
+
+    // 定义sqlist数组，将查询到的数据用additems添加到combobox中
+    QList<QString> sqllist;
+    QString country;
+    while (sqlsearch.next())
+    {
+        country = sqlsearch.value(0).toString();
+        sqllist.append(country);
+    }
+    ui->comboBox_2->addItems(sqllist);
+}
+
--- purchase.h
+++ purchase.h
@@ -15,6 +15,11 @@ public:
     explicit purchase(QWidget *parent = nullptr);
     ~purchase();
 
+private slots:
+    // void on_comboBox_activated(int index);
+
+    void on_comboBox_currentTextChanged(const QString &arg1);
+
 private:
     void setvalue();
 
